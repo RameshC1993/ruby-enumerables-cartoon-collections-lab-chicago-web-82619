af6c711e43e3a9261588f8ffde5959e6115f7ecc
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-22 00:41:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,29 +1,8 @@
-
-
-
-def summon_captain_planet(array)# code an argument here
-  
-  array.collect{|x| x.capitalize + "!"}
-# Your code here
+def roll_call_dwarves(array)
+  counter = 0
+  array.each do |x|
+    puts "#{counter + 1}. #{x}"
+    counter += 1
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese(strings)# code an argument here
-  cheddar_included = strings.include? ("cheddar")
-  gouda_included = strings.include? ("gouda")
-  camembert_included = strings.include? ("camebert")
-
-    if cheddar_included == true
-      return "cheddar"
-    elsif gouda_included == true
-      return "gouda"
-    elsif camembert_included == true
-      return "camebert"
-    else 
-      return "nil"
-    end# the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
