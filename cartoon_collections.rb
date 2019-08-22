e7cee7ac16313914617c95dd2305f0533dbed6a4
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-22 00:25:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,19 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(# code an argument here
+  cheddar_included = strings.include? ("cheddar")
+  gouda_included = strings.include? ("gouda")
+  camembert_included = strings.include? ("camebert")
+
+    if cheddar_included == true
+      return "cheddar"
+    elsif gouda_included == true
+      return "gouda"
+    elsif camembert_included == true
+      return "camebert"
+    else 
+      return "nil"
+    end# the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
