72ffe9cb17ed0736c1b9883593db65ec27211c51
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-22 00:46:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,3 +14,20 @@ def long_planeteer_calls(array)
   array.any? {|x| x.length > 4}
 end
 
+def find_the_cheese(strings)
+  
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheddar_included = strings.include? ("cheddar")
+  gouda_included = strings.include? ("gouda")
+  camembert_included = strings.include? ("camebert")
+
+    if cheddar_included == true
+      return "cheddar"
+    elsif gouda_included == true
+      return "gouda"
+    elsif camembert_included == true
+      return "camebert"
+    else 
+      return "nil"
+    end
+end
